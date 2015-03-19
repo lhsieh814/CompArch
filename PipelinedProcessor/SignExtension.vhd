75c2bbf3c66252e6bf09839749ed1fa3f1edee58
Gavin Zhou <gavinzhou10c@gmail.com>
2015-03-19 13:37:04 -0400

    [Add] Add SignExtension

---
+++ PipelinedProcessor/SignExtension.vhd
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+Entity SignExtension is
+	Port(
+		A	: in std_logic_vector(15 downto 0);
+		Y	: out std_logic_vector(31 downto 0);
+	);
+	End;
+
+Architecture behave of SignExtension is 
+	begin
+		Y <= X"0000" & A when A(15)='0' else X"ffff" &A;
+	end;
